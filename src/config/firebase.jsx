b6c57bc67dbc3fc4cd592d8cbe0a87c162006716
Eshwar J <91658499+Eshwar1509@users.noreply.github.com>
2024-05-12 18:10:07 +0530

    Update firebase.jsx

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZM0URWOBtk_xl8Thmq5hctClfhVmlWCk",
-  authDomain: "fir-course-c67a0.firebaseapp.com",
-  projectId: "fir-course-c67a0",
-  storageBucket: "fir-course-c67a0.appspot.com",
-  messagingSenderId: "339296091094",
-  appId: "1:339296091094:web:74cd8ed7ac72c501a60277",
-  measurementId: "G-KFGS5S34RG",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "7",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
